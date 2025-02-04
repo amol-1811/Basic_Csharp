f1657664a1266d52cf7b9530f815c8a6e49e8410
Amol Gosavi <gosaviamol1811@gmail.com>
2025-02-04 12:11:12 +0530

    minor changes done

--- Unique_Char.cs
+++ Unique_Char.cs
@@ -21,21 +21,16 @@ namespace Basic_Csharp
 
         static bool HasAllUniqueCharacters(string str)
         {
-            // Use a HashSet to store characters
             HashSet<char> charSet = new HashSet<char>();
 
-            // Iterate through each character in the string
             foreach (char c in str)
             {
-                // Try to add the character to the HashSet
                 if (!charSet.Add(c))
                 {
-                    // If Add returns false, the character is already in the HashSet
                     return false;
                 }
             }
 
-            // If all characters are added successfully, they are unique
             return true;
         }
     }

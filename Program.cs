6d7c047a529a03c55c896799d1beb19b1686a368
Amol Gosavi <gosaviamol1811@gmail.com>
2025-02-04 11:21:46 +0530

    First prime number program

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+class Program
+{
+    static void PrintPrimes(int n)
+    {
+        for (int i = 2; i <= n; i++)
+        {
+            if (IsPrime(i))
+            {
+                Console.Write(i + " ");
+            }
+        }
+    }
+
+    static bool IsPrime(int num)
+    {
+        if (num < 2)
+            return false;
+
+        for (int i = 2; i * i <= num; i++)
+        {
+            if (num % i == 0)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+    static void Main(string[] args)
+    {
+        Console.Write("Enter a number : ");
+        int n = int.Parse(Console.ReadLine());
+
+        Console.WriteLine($"Prime numbers up to {n} are:");
+        PrintPrimes(n);
+    }
+}
